3a11c19ce253e868055e140fcb8e9bcc208f0a81
verobueno <veronica_bueno@yahoo.com.ar>
2018-04-24 01:17:43 -0300

    Add files via upload

--- Clase_10/EstructurasYrelacionesEnC/GerFlix2.0/usuarioSerie.c
+++ Clase_10/EstructurasYrelacionesEnC/GerFlix2.0/usuarioSerie.c
@@ -11,7 +11,7 @@ void CrearListadoUsuariosYSeries(eUsuarioSerie misRelaciones[])
     int usuario[9]={100,100,100,100,101,101,101,102,102};
     int serie[9]={1,2,3,4,1,2,3,1,4};
 
-    for(i=0;i<10;i++)
+    for(i=0;i<9;i++)
     {
       misRelaciones[i].idSerie=serie[i];
       misRelaciones[i].idUsuario=usuario[i];
